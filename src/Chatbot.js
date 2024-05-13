1095041678e7e20e47042dfc79ad3343abc6c5b6
klu_2200031618 <kk258786@gmail.com>
2024-05-13 15:33:37 +0530

    Footer is added and this is completed

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -141,6 +141,9 @@ function Chatbot() {
             <button onClick={SenderMessageGenerator}>Send</button>
           </div>
       </div>
+      <footer>
+      <p><a href="https://github.com/AzmatullaKhan">Copyright © All Rights Reserved 2024 Azmatulla Khan ®</a></p>
+      </footer>
     </div>
   );
 }
